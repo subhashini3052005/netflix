671f8ba4880f513fd8fcd0afbf698402d2e0f37e
subhashini3052005 <ssubhashini575@gmail.com>
2025-12-23 05:46:34 +0530

    initial login page

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Frontpage from './pages/frontpage';
+
+function App() {
+  return (
+    <div className="App">
+      <Frontpage>
+        
+      </Frontpage>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/pages/frontpage.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import "./frontpage.css"
+import netflix from "../asset/netflix.png"
+const Frontpage = () => {
+  return (
+    <div className="frontpage">
+      <div className="b1">
+        <img src={netflix} className='icon' alt="netflix" />
+        <div className="right">
+        <select className='lang'>
+          <option value="">English</option>
+          <option value="option1">Hindi</option>
+          <option value="option2">Telugu</option>
+        </select>
+        <button className='signin'>Sign In</button>
+        </div>
+      </div>
+      <div className="header">
+        <h1>Unlimited Movies,<br />Shows & Mores</h1>
+        <h2>Start at $149,Cancel at anytime</h2>
+        <p>Ready to watch?enter your email to create or restart your memnership</p>     
+       </div>
+       <div className="b2">
+        <input type="email" placeholder='' required />
+        <label >Email address</label>
+        <button className='getstarted'>Get Started</button>
+         </div>
+        
+    </div>
+  )
+}
+export default Frontpage;
